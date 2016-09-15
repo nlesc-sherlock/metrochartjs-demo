787517c1b05ed62d259643ae20058ed910aabbc1
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-09-15 14:53:32 +0200

    added the index.html and the main script to the demo website

--- src/main-script.js
+++ src/main-script.js
@@ -1,39 +1,44 @@
-let metrochart: MetroChart;
 
 
 
+var chart,
+    datasetNames,
+    randomIndex,
+    showDataSet,
+    options;
 
-let datasetNames = [
-    'tintin',
-    'metrolines'
+datasetNames = [
+    "tintin",
+    "metrolines"
 ];
-let randomIndex = Math.floor(Math.random() * datasetNames.length);
-let showDataSet: string = datasetNames[randomIndex];
+
+randomIndex = Math.floor(Math.random() * datasetNames.length);
+showDataSet = datasetNames[randomIndex];
 
 switch (showDataSet) {
-    case 'metrolines': {
-            let options: Options = {
+    case "metrolines": {
+            options = {
                 enableTimeAxis: false,
                 charge: -5,
                 linkStrength: 0.5,
                 linkDistance: 50,
                 gravity: 0.0005,
                 colors: [
-                    {name:'green', hexcode: '#008000'},
-                    {name:'orange', hexcode: '#FF8000'},
-                    {name:'red', hexcode: '#FF0000'},
-                    {name:'yellow', hexcode: '#FFFF00'}
+                    {name: "green", hexcode: "#008000"},
+                    {name: "orange", hexcode: "#FF8000"},
+                    {name: "red", hexcode: "#FF0000"},
+                    {name: "yellow", hexcode: "#FFFF00"}
                 ],
                 stationShapeRadius: 5
             };
-            metrochart = new MetroChart('#metrochart', window.location.href + '/data/metrolines.json', options);
+            chart = new metrochart.MetroChart("#metrochart", window.location.href + "/data/metrolines.json", options);
         }
         break;
-    case 'tintin': {
-            let options: Options = {
+    case "tintin": {
+            options = {
                 charge: 0,
                 linkStrength: 0.0
             };
-            metrochart = new MetroChart('#metrochart', window.location.href + '/data/tintin-the-black-island.json', options);
+            chart = new metrochart.MetroChart("#metrochart", window.location.href + "/data/tintin-the-black-island.json", options);
         }
 }
